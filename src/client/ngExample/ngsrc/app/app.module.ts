e11009529db8af0c47674752ba768ae2911fc9da
Jens Bodal <jensbodal@gmail.com>
2017-06-15 15:52:18 -0700

    bootstrap hybrid app

--- src/client/ngExample/ngsrc/app/app.module.ts
+++ src/client/ngExample/ngsrc/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    UpgradeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+  ]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) { }
+
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['angularSeed'], {strictDi: true})
+  }
+}
